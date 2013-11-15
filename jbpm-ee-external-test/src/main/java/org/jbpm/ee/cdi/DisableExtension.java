f6b6106694689e5d4c2334566c11592bcd1cc271
Brad Davis <bradsdavis@gmail.com>
2013-11-15 14:28:55 -0500

    Added in a logger to the disableextension.

--- jbpm-ee-external-test/src/main/java/org/jbpm/ee/cdi/DisableExtension.java
+++ jbpm-ee-external-test/src/main/java/org/jbpm/ee/cdi/DisableExtension.java
@@ -4,18 +4,22 @@ import javax.enterprise.event.Observes;
 import javax.enterprise.inject.spi.Extension;
 import javax.enterprise.inject.spi.ProcessAnnotatedType;
 
+import org.slf4j.Logger;
+
 /**
  * Disables the processing of jBPM service annotations.
  * @author bradsdavis
  *
  */
 public class DisableExtension implements Extension {
+	private static final Logger LOG = org.slf4j.LoggerFactory.getLogger(DisableExtension.class);
 
 	<T> void processAnnotatedType(@Observes ProcessAnnotatedType<T> pat) {
 		Class c = pat.getAnnotatedType().getJavaClass();
 		String qualifiedName = c.getName();
 
 		if (qualifiedName.startsWith("org.jbpm.services")) {
+			LOG.info("Disabling CDI: "+qualifiedName);
 			pat.veto();
 		}
 	}
