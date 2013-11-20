c0c1f9dee1fbb771683bcaaf13695645625152c9
Adam Baxter <abaxter@redhat.com>
2013-11-20 17:55:10 -0500

    Added simple log handler (probably will be removed)

---
+++ jbpm-ee-services/src/main/java/org/jbpm/ee/handlers/LogHandler.java
@@ -0,0 +1,28 @@
+package org.jbpm.ee.handlers;
+
+import java.util.Map;
+
+import org.kie.api.runtime.process.WorkItem;
+import org.kie.api.runtime.process.WorkItemHandler;
+import org.kie.api.runtime.process.WorkItemManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LogHandler implements WorkItemHandler {
+
+	private static final Logger LOG = LoggerFactory.getLogger(LogHandler.class);
+	
+	@Override
+	public void executeWorkItem(WorkItem workItem, WorkItemManager manager) {
+		// TODO Auto-generated method stub
+		Map<String, Object> parameterMap = workItem.getParameters();
+		LOG.info(parameterMap.toString());
+		manager.completeWorkItem(workItem.getId(), null);
+	}
+
+	@Override
+	public void abortWorkItem(WorkItem workItem, WorkItemManager manager) {
+		// TODO Auto-generated method stub
+	}
+
+}
