ce6cfa80a5accea31ec48d81a2626b651a9f90c6
Matyas Danter <mdanter@gmail.com>
2013-11-20 11:42:44 -0500

    refactoring Service projects for archetype generation

---
+++ jbpm-ee-external-soap/src/main/java/org/jbpm/ee/archetype/SoapService.java
@@ -0,0 +1,65 @@
+
+package org.jbpm.ee.archetype;
+
+import javax.jws.WebService;
+
+import org.jbpm.ee.client.ClientException;
+import org.jbpm.ee.client.SoapClientFactory;
+import org.jbpm.ee.services.ProcessService;
+import org.jbpm.ee.services.TaskService;
+import org.jbpm.ee.services.WorkItemService;
+
+@WebService(targetNamespace="http://jbpm.org/v6/SoapService/wsdl", serviceName="SOAPService")
+public class SoapService extends BaseService {
+
+	private TaskService cachedTaskService = null;
+	private ProcessService cachedProcessService = null;
+	private WorkItemService cachedWorkItemService = null;
+	
+	private static final String PROCESS_SERVICE_URL = "http://localhost:8080/jbpm-ee-services/ProcessService?wsdl";
+	private static final String TASK_SERVICE_URL = "http://localhost:8080/jbpm-ee-services/TaskService?wsdl"; 
+	private static final String WORK_ITEM_SERVICE_URL = "http://localhost:8080/jbpm-ee-services/WorkItemService?wsdl"; 
+	
+	/**
+	 * Creates the ProcessService & caches the instance for reuse.  
+	 */
+	@Override
+	protected ProcessService getProcessService() {
+		if(cachedProcessService == null) {
+			try {
+				cachedProcessService = SoapClientFactory.getProcessService(PROCESS_SERVICE_URL); 
+			} catch (ClientException e) {
+				throw new RuntimeException("Unable to create process service from ["+PROCESS_SERVICE_URL+"]. Please validate URL.", e);
+			}
+		}
+		return cachedProcessService;
+		
+	}
+
+	/**
+	 * Creates the TaskService & caches the instance for reuse.  
+	 */
+	@Override
+	protected TaskService getTaskService() {
+		if(cachedTaskService == null) {
+			try {
+				cachedTaskService = SoapClientFactory.getTaskService(TASK_SERVICE_URL);
+			} catch (ClientException e) {
+				throw new RuntimeException("Unable to create TaskService from ["+TASK_SERVICE_URL+"]. Please validate URL.", e);
+			}
+		}
+		return cachedTaskService;
+	}
+
+	@Override
+	protected WorkItemService getWorkItemService() {
+		if(cachedWorkItemService == null) {
+			try {
+				cachedWorkItemService = SoapClientFactory.getWorkItemService(WORK_ITEM_SERVICE_URL);
+			} catch (ClientException e) {
+				throw new RuntimeException("Unable to create WorkItemService from ["+WORK_ITEM_SERVICE_URL+"]. Please validate URL.", e);
+			}
+		}
+		return cachedWorkItemService;
+	}
+}
