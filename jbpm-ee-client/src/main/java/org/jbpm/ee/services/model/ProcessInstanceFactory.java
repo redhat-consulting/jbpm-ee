2abc4c0ef705c1068a3732066953f9aeef8f7b80
Matyas Danter <mdanter@gmail.com>
2013-11-21 12:34:04 -0500

    Added converter methods for CommandResponse in the MDB, and converter helper methods to Process InstanceFactory

--- jbpm-ee-client/src/main/java/org/jbpm/ee/services/model/ProcessInstanceFactory.java
+++ jbpm-ee-client/src/main/java/org/jbpm/ee/services/model/ProcessInstanceFactory.java
@@ -1,5 +1,10 @@
 package org.jbpm.ee.services.model;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
 import org.kie.api.runtime.process.ProcessInstance;
 import org.kie.api.runtime.process.WorkItem;
 
@@ -27,5 +32,18 @@ public class ProcessInstanceFactory {
 			return null;
 		}
 		return new org.jbpm.ee.services.model.process.WorkItem(instance);
+	}
+
+	public static Collection<ProcessInstance> convertProcessInstances(Collection<ProcessInstance> instances) {
+		if(instances == null){
+			return null;
+		}
+		
+		List<ProcessInstance> result = new LinkedList<ProcessInstance>();
+		for(ProcessInstance instance : instances) {
+			result.add(convert(instance));
+		}
+		
+		return Collections.unmodifiableList(result);
 	}
 }
