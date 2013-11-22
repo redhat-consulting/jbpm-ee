1c302775dc998a8b5e827360c57391115b386adb
Adam Baxter <abaxter@redhat.com>
2013-11-22 13:31:16 -0500

    Updated to 6.0.0.Final

--- jbpm-ee-services/src/main/java/org/jbpm/ee/services/ejb/startup/ResourceManagerBean.java
+++ jbpm-ee-services/src/main/java/org/jbpm/ee/services/ejb/startup/ResourceManagerBean.java
@@ -32,9 +32,7 @@ public class ResourceManagerBean {
 	}
 	
 	@Produces
-	public UserGroupCallback getUserGroupCallback() {
-		// will be JAASUserGroupCallbackImpl() once we switch to LDAP
-		// JAASUserGroupCallbackImpl will require a security context, which means changing quite a bit
+	public UserGroupCallback getInternalUserGroupCallback() {
 		return new JBossUserGroupCallbackImpl("classpath:/usergroup.properties");
 	}
 }
