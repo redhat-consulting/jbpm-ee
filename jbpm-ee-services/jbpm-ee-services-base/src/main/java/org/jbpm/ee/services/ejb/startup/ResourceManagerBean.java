243507019c6095a8ff3636887bd78351eb0805ed
Matyas Danter <mdanter@gmail.com>
2013-12-13 16:23:05 -0500

    Added LDAP and File based User Group Callback Implementation Functionality.

--- jbpm-ee-services/jbpm-ee-services-base/src/main/java/org/jbpm/ee/services/ejb/startup/ResourceManagerBean.java
+++ jbpm-ee-services/jbpm-ee-services-base/src/main/java/org/jbpm/ee/services/ejb/startup/ResourceManagerBean.java
@@ -1,5 +1,6 @@
 package org.jbpm.ee.services.ejb.startup;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
@@ -11,39 +12,63 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceContext;
 
 import org.jbpm.services.task.identity.JBossUserGroupCallbackImpl;
+import org.jbpm.services.task.identity.LDAPUserGroupCallbackImpl;
 import org.kie.internal.task.api.UserGroupCallback;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Provides CDI Resources
+ * 
  * @author abaxter
- *
+ * 
  */
 @Startup
-@Singleton(name="ResourceManager")
+@Singleton(name = "ResourceManager")
 public class ResourceManagerBean {
 
-	@PersistenceContext(name="org.jbpm.persistence.jpa", unitName="org.jbpm.persistence.jpa")
-	private EntityManager em;
+	private static final Logger LOG = LoggerFactory.getLogger(ResourceManagerBean.class);
 	
+	@PersistenceContext(name = "org.jbpm.persistence.jpa", unitName = "org.jbpm.persistence.jpa")
+	private EntityManager em;
+
 	@Produces
 	public EntityManagerFactory getEntityManagerFactory() {
 		return em.getEntityManagerFactory();
 	}
-	
+
 	@Produces
 	public EntityManager getEntityManager() {
 		return em;
 	}
-	
+
 	@Produces
 	public UserGroupCallback getUserGroupCallback() throws IOException {
-		// will be JAASUserGroupCallbackImpl() once we switch to LDAP
-		// JAASUserGroupCallbackImpl will require a security context, which means changing quite a bit
+
+		Properties props = new Properties();
 		
-		Properties prop = new Properties();
+		if (System.getProperty("jbpm.ee.user.group.callback.ldap.properties") != null) {
+			
+			
+			String ldapFileLocation = System.getProperty("jbpm.ee.user.group.callback.ldap.properties");
+			LOG.info("Loading LDAP file from location: "+ ldapFileLocation);
+			
+			props.load(new FileInputStream(ldapFileLocation));
+			
+			return new LDAPUserGroupCallbackImpl(props);
+			
+		}else if (System.getProperty("jbpm.ee.user.group.callback.ldap.properties") != null) {
+			
+			String fileLocation = System.getProperty("jbpm.ee.user.group.callback.file.properties");
+			LOG.info("Loading Properties file from location: "+ fileLocation);
+			
+			props.load(new FileInputStream(fileLocation));
+
+			return new JBossUserGroupCallbackImpl(props);
+			
+		}else{
+			throw new IllegalStateException("Please configure a User Group Callback Implementation.");
+		}
 		
-		prop.load(getClass().getClassLoader().getResourceAsStream("usergroup.properties"));
-		
-		return new JBossUserGroupCallbackImpl(prop);
 	}
 }
